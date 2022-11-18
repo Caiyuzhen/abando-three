09702f338d987c0077c3177e857d58accfba5c03
yuzhen <caiyuzhen00@gmail.com>
2022-11-18 14:51:49 +0800

    ⚡️

---
+++ src/components/MainView.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const MainView = () => {
+	return (
+		<>
+			<div className="experience">
+				<canvas className="experience-canvas"></canvas>
+			</div>
+		</>
+	)
+}
+
+export default MainView
